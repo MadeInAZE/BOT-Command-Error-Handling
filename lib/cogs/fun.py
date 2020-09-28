83aca27d8ff88a08cd930c58e8784e7d7efdf1c8
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2020-09-28 21:47:26 +0300

    Error Handling

--- lib/cogs/fun.py
+++ lib/cogs/fun.py
@@ -2,6 +2,7 @@ from random import choice, randint
 from typing import Optional
 
 from discord import Member, File
+from discord import HTTPException
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -10,27 +11,42 @@ class Fun(Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Say commands go here
     @command(name="hello", aliases=["hi"])
     async def say_hello(self, ctx):
         await ctx.send(f"{choice(('Hello', 'Hi', 'Hey', 'Hiya'))} {ctx.author.mention} !")
 
+    # Dice commands go here
     @command(name="dice", aliases=["roll"])
     async def roll_dice(self, ctx, die_string: str):
         dice, value = (int(term) for term in die_string.split("d"))
-        rolls = [randint(1, value) for i in range(dice)]
+        if dice <= 25:
+            rolls = [randint(1, value) for i in range(dice)]
 
-        await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
+            await ctx.send(" + ".join([str(r) for r in rolls]) + f" = {sum(rolls)}")
 
+        else:
+            await ctx.send("Please use less than 25 dice!")
+
+    # Slap commands go here
     @command(name="slap", aliases=["hit"])
     async def slap_member(self, ctx, member: Member, *, reason: Optional[str] = "no reason"):
         await ctx.send(f"{ctx.author.mention} slapped {member.mention}  {reason}!")
         await ctx.send(file=File("./data/images/slap2.gif"))
 
+    # Slap Error Handling Test goes here
+    @slap_member.error
+    async def slap_member_error(self, ctx, exc):
+        if isinstance(exc, BadArgument):
+            await ctx.send("WTF?")
+
+    # Echo commands go here
     @command(name="echo", aliases=["say"])
     async def echo_message(self, ctx, *, message):
         await ctx.message.delete()
         await ctx.send(message)
 
+    # Running all the commands included within the fun.py file go here
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
